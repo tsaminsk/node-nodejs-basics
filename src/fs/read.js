7401dc0e6f44b55fe77549b00f7e78bf92e02063
Talai <tsa@tut.by>
2022-12-04 16:49:41 +0300

    create fs block

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import * as fs from 'fs';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const fsPromises = fs.promises;
+
+const targetPath = `${__dirname}/files/fileToRead.txt`;
+const errorMsg = 'File system operation failed';
+
 const read = async () => {
-    // Write your code here 
+  fsPromises.readFile(targetPath, 'utf-8', { flag: 'wx' })
+    .then(data => console.log(data))
+    .catch(() => {
+      throw new Error(errorMsg);
+    })
 };
 
 await read();
