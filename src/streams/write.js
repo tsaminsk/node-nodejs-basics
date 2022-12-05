a3850e7c0d81a39ee1bd674dc0228e2224406f54
Talai <tsa@tut.by>
2022-12-05 16:46:22 +0300

    create stream block

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import { createWriteStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { pipeline } from 'node:stream/promises';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const { stdin } = process;
+
 const write = async () => {
-    // Write your code here 
+  try {
+    await pipeline(
+      stdin,
+      createWriteStream(join(__dirname, 'files', 'fileToWrite.txt'))
+    );
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 await write();
