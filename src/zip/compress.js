4579707e3b1b232d3eb536844f4f2023f770b1c6
Talai <tsa@tut.by>
2022-12-05 18:58:18 +0300

    create zip block

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import fs from 'node:fs';
+import zlib from 'node:zlib';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = zlib.createGzip();
+    const fileToZip = fs.createReadStream(path.join(__dirname, 'files', 'fileToCompress.txt'));
+    const archive = fs.createWriteStream(path.join(__dirname, 'files', 'archive.gz'));
+    fileToZip.pipe(gzip).pipe(archive);
+    console.log('File successfully compressed!');
 };
 
 await compress();
